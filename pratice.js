b533099a8f26ba278fa1a96826a7fa51189aa49b
Prince211105 <princepatel211103@gmail.com>
2024-03-11 15:25:56 +0530

    container config

---
+++ pratice.js
@@ -0,0 +1,104 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+
+const PORT = 2000
+app.use(express.json())
+app.use(cors())
+
+let Phonebook = [
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    },
+]
+
+app.get('/api/data', (req, res) => {
+    res.json(Phonebook)
+})
+
+app.get('/api/data/:id', (req, res) => {
+    let id = Number(req.params.id)
+    let data = Phonebook.find(e => e.id === id)
+    if (data) {
+        res.json(data)
+    }
+    else {
+        res.status(404).json({
+            error: 404,
+            message: "Data is not found"
+        })
+    }
+})
+
+
+app.delete('/api/data/:id', (req, res) => {
+    let id = Number(req.params.id)
+    Phonebook = Phonebook.filter(item => item.id !== id)
+    res.status(404).end()
+})
+
+app.use((req, res, next) => {
+    const { name, number } = req.body
+
+    if (!name || !number) {
+        return res.status(404).json({
+            error: 404,
+            message: "name and number is missing"
+        })
+    }
+    next()
+})
+
+app.use((req, res, next) => {
+    const { name } = req.body
+    const isadd = Phonebook.find(phone => phone.name === name)
+
+    if (isadd) {
+        return res.status(404).json({
+            error: 404,
+            message: "must be name unique"
+        })
+    }
+    next()
+})
+
+
+
+const newid = () => {
+    let maxId = Phonebook.length > 0 ? Math.max(...Phonebook.map(e => e.id)) : 0
+    return maxId + 1;
+}
+
+app.post('/api/data', (req, res) => {
+    const { name, number } = req.body
+
+    const Phone = {
+        name: name,
+        number: number,
+        id: newid()
+    }
+
+    Phonebook.push(Phone)
+    res.json(Phone)
+})
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+})
