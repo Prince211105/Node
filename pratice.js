2c1028663720082329954f1b0604fe2f6d6f3710
Prince211105 <princepatel211103@gmail.com>
2024-03-12 12:58:54 +0530

    container config

--- pratice.js
+++ pratice.js
@@ -7,7 +7,26 @@ app.use(express.json())
 app.use(cors())
 
 let Phonebook = [
-
+    {
+        "name": "Arto Hellas",
+        "number": "040-123456",
+        "id": 1
+    },
+    {
+        "name": "Ada Lovelace",
+        "number": "39-44-5323523",
+        "id": 2
+    },
+    {
+        "name": "Dan Abramov",
+        "number": "12-43-234345",
+        "id": 3
+    },
+    {
+        "name": "Mary Poppendieck",
+        "number": "39-23-6423122",
+        "id": 4
+    },
 ]
 
 app.use(express.static('dist'))
