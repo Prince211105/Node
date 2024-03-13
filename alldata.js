7ed3b41aff2c72ccbf775bfa3ceb59e6cb9979cc
Prince211105 <princepatel211103@gmail.com>
2024-03-13 10:30:54 +0530

    container config

--- alldata.js
+++ alldata.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const PORT = 2001;
 app.use(express.json())
-const mongoose = require('mongoose')
+// const mongoose = require('mongoose')
 
 let Phonebook = [
     {
